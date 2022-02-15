3af2316c9c2bd0529754778f3dca5779b565feca
hrishikeshabcd123 <48756386+hrishikeshabcd123@users.noreply.github.com>
2022-02-16 09:54:26 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,23 +1,30 @@
-from flask import Flask,render_template
-import requests
+from flask import Flask, render_template, request, send_file
+import ipaddress
+import zipfile
+from pathlib import Path
 
 app = Flask(__name__)
 
+[...]
 
-@app.route('/')
-def my_form():
-    return render_template('my-form.html')
+# generate configuration when data is received from generator form
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    [...] some code, input checks, text manipulation
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
-    app.run(debug=TRUE)
-    
-
-
+        # check folder content for .cfg files and add them to a zip archive
+        with zipfile.ZipFile(temp_zipfile, mode='w') as z:
+            for file in base_path.iterdir():
+                if file.suffix == ".cfg":
+                    try:
+                        z.write(file, file, zipfile.ZIP_DEFLATED)
+                    except:
+                        continue
+        # send the zip file to the user
+        return send_file(temp_zipfile, as_attachment=True, mimetype='application/zip',attachment_filename="configurations.zip")
+    # if HA is not used, send a cfg file directly
+    elif result["cluster"] == "no":
+        configfile = config_filepath + result["HOSTNAME"] + ".cfg"
+        with open(configfile, "w+") as cfg:
+            cfg.write(content)
+        return send_file(configfile, as_attachment=True,attachment_filename='configurations.cfg')
