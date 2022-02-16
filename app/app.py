e631c2fc32ddd9dde4141ab6e2cc827baefcd7ff
hrishikeshabcd123 <48756386+hrishikeshabcd123@users.noreply.github.com>
2022-02-16 11:32:51 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,13 @@
-from flask import Flask,render_template,request
- 
+from flask import Flask
 app = Flask(__name__)
- 
-@app.route('/form')
-def form():
-    return render_template('form.html')
- 
-@app.route('/data/', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
+
+@app.route('/')
+def hello_world():
+    return 'Flask: Hello World from Docker'
+
+@app.route('/api')
+def rest_hello_world():
+    return '{"id":1,"message":"Flask: Hello World from Docker"}'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
