3bee12a7161c7cffd9e4a9973bc01d73a3cfe6ea
hrishikeshabcd123 <48756386+hrishikeshabcd123@users.noreply.github.com>
2022-02-16 11:01:41 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,17 +1,15 @@
-from flask import Flask,render_template
-import socket
-
+from flask import Flask,render_template,request
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def index():
-    try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
-    except:
-        return render_template('error.html')
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
