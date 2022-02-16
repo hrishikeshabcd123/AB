9ec1229ee9e3c9850f22e73f894e292c135476a2
hrishikeshabcd123 <48756386+hrishikeshabcd123@users.noreply.github.com>
2022-02-16 11:37:37 +1100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,7 @@
-from flask import Flask
+import flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask: Hello World from Docker'
-
-@app.route('/api')
-def rest_hello_world():
-    return '{"id":1,"message":"Flask: Hello World from Docker"}'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    return render_template('hello.html', version = flask.__version__)
